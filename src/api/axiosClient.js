e4462cb97a3ffe1acc8f5459664077beb304b17b
ngthtrong204 <ngthtrong204@gmail.com>
2023-06-24 22:48:07 +0700

    to be continiuuuuu...

--- src/api/axiosClient.js
+++ src/api/axiosClient.js
@@ -4,7 +4,7 @@ const axiosClient = axios.create({
    baseURL: "https://airbnbnew.cybersoft.edu.vn/api/",
    headers: {
       TokenCybersoft:
-         "yJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ0ZW5Mb3AiOiJCb290Y2FtcCA0MiIsIkhldEhhblN0cmluZyI6IjEwLzEwLzIwMjMiLCJIZXRIYW5UaW1lIjoiMTY5Njg5NjAwMDAwMCIsIm5iZiI6MTY2NzA2MjgwMCwiZXhwIjoxNjk3MDQzNjAwfQ.g_aUM-jnWQ1i_eCbjNfvNxudUdUPpfC36068g5o9Ung",
+         "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ0ZW5Mb3AiOiJCb290Y2FtcCA0MiIsIkhldEhhblN0cmluZyI6IjEwLzEwLzIwMjMiLCJIZXRIYW5UaW1lIjoiMTY5Njg5NjAwMDAwMCIsIm5iZiI6MTY2NzA2MjgwMCwiZXhwIjoxNjk3MDQzNjAwfQ.g_aUM-jnWQ1i_eCbjNfvNxudUdUPpfC36068g5o9Ung",
    },
 })
 axiosClient.interceptors.request.use((config) => {
